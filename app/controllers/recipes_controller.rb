016db5699fcd3944e29dc151b4bfe331474f0554
ltviet100 <ltviet100@gmail.com>
2019-02-25 14:55:14 +0700

    Complete TDD for show action of recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show]
   def index
     @recipes = Recipe.all
   end
+
+  def show
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
 end

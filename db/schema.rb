4d8dd67b330a0cff7322abdfe622ff7d74a5b420
ltviet100 <ltviet100@gmail.com>
2019-02-25 11:35:21 +0700

    Generate model, table chefs and recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_25_042808) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "chefname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "desciption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "chef_id"
+  end
 
 end

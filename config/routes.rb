0b65e2ab2b1b8623f1d1a49aeec612f7f355c89f
ltviet100 <ltviet100@gmail.com>
2019-02-25 22:47:58 +0700

    Signup form and test for signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/home', to: 'pages#home'
   resources :recipes
+  resources :chefs, except: [:new]
+  get 'signup', to:'chefs#new'
 end

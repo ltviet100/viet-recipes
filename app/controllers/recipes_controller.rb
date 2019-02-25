77b1ec90591b7cc6bd26e0b7cb830f7f2788f3f8
ltviet100 <ltviet100@gmail.com>
2019-02-25 16:33:36 +0700

    Complete add new recipe with test

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,8 +11,23 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:notice] = "Recipe was created successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
   private
     def set_recipe
       @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :description)
     end
 end

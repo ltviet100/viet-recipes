642f28a42f9875fb9027fad21d0e2bba602096c9
ltviet100 <ltviet100@gmail.com>
2019-02-25 23:10:05 +0700

    Complete signup and test for signup

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,28 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show, :edit, :update, :destroy]
   def new
     @chef = Chef.new
   end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:notice] = "Welcome #{@chef.chefname} to the application"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+    def chef_params
+      params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
+    end
+
+    def set_chef
+      @chef = Chef.find(params[:id])
+    end
 end
